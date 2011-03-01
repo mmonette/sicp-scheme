4bcd75a0e8b06b7364e771fec70e5bf7edddc355
Mike Monette <mmonette@gmail.com>
2011-03-01 15:02:19 -0500

    First version of Chapter 01

---
+++ chapter01.scm
@@ -0,0 +1,14 @@
+;; exercise 1.3
+(define (square x) (* x x))
+(define (sum-of-squares x y) (+ (square x) (square y)))
+(define (ss-two-largest x y z)
+  (cond ((> x y) (sum-of-squares x (max y z)))
+        (else (sum-of-squares y (max x z)))))
+
+;; exercise 1.5
+(define (p) (p))
+(define (test x y)
+  (if (= x 0)
+      0
+      y))
+
